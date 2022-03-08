83be765adfe7aee8f898825966aca58678c6d8cb
Robson Ferreira <tech.rob@icloud.com>
2022-03-08 17:47:04 -0500

    updates and fixes

--- data/CH04/04_02.sql
+++
@@ -1,7 +0,0 @@
-﻿select
-   e1.last_name,
-   e1.salary,
-   e1.department_id,
-   (select avg(salary) from  data_sci.employees e2 where e1.department_id = e2.department_id)
-from
-   data_sci.employees e1
--- data/CH04/04_04.sql
+++
@@ -1,6 +0,0 @@
-﻿select
-   department_id
-from
-   data_sci.employees e1
-where
-   (select max(salary) from data_sci.employees e2) = e1.salary
